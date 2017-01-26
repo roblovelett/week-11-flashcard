fcad403d2a52e6f51697b13ddc8a8c8dbfdfc585
Rob Lovelett <rlovelett@gmail.com>
2017-01-25 18:14:50 -0600

    started constructors

--- cloze.js
+++ cloze.js
@@ -6,9 +6,49 @@ clozeCard();
 
 function clozeCard() {
     var prompt = inquirer.createPromptModule();
-    var questions = cloze;
-    console.log(questions);
+    var questions = []; //init
+    var keys = Object.keys(cloze);
+    for (i=0; i < keys.length; i++) {
+        var question = cloze[keys[i]];
+        console.log(/*JSON.stringify(*/question.answer/*)*/);
+    };
 
+/*
+    for (i=0; i < keys.length; i++) {
+        var question = cloze[keys[i]];
+        prompt([
+            {
+                name: JSON.stringify(question.answer),
+                message: "..." + JSON.stringify(question.text)
+            }
+        ])
+    };
+    console.log(keys.length);
+    console.log(cloze[keys[0]]);
+    console.log(cloze[keys[1]]);
+    console.log(cloze[keys[2]]);
+*/
+
+    //console.log(keys);
+    
+    /*
+    .push
+    {
+        type: 'input',
+        name: 'firstName',
+        message: 'What is your first name?'
+    }, {
+        name: 'operatingSystem',
+        message: 'What is your operating System?',
+        default: 'Windows'
+}, {
+  name: 'favoriteColor',
+  message: 'What is your favorite rainbow Color?',
+  type: 'list'
+  choices: ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet']
+}];
+    prompt(questions).then();
+*/
 
 /*
     var keys = Object.keys(cloze);
